541c3e34de3dd5bdba71f0940976197adb32845f
Tim Kennedy <KennedyTim@outlook.com>
2022-09-09 16:47:44 -0500

    Added button for ReadMe file to About dialog

--- MyLauncher/Dialogs/About.xaml.cs
+++ MyLauncher/Dialogs/About.xaml.cs
@@ -15,6 +15,15 @@ public partial class About : UserControl
     }
 
     #region License click
+    /// <summary>
+    /// Handles the Click event of the BtnLicense control.
+    /// </summary>
+    /// <param name="sender">
+    /// The source of the event.
+    /// </param>
+    /// <param name="e">
+    /// The <see cref="RoutedEventArgs" /> instance containing the event data.
+    /// </param>
     private void BtnLicense_Click(object sender, RoutedEventArgs e)
     {
         string dir = AppInfo.AppDirectory;
@@ -23,6 +32,15 @@ public partial class About : UserControl
     #endregion License click
 
     #region URL click
+    /// <summary>
+    /// Called when [navigate].
+    /// </summary>
+    /// <param name="sender">
+    /// The sender.
+    /// </param>
+    /// <param name="e">
+    /// The <see cref="RequestNavigateEventArgs" /> instance containing the event data.
+    /// </param>
     private void OnNavigate(object sender, RequestNavigateEventArgs e)
     {
         Process p = new();
@@ -32,4 +50,21 @@ public partial class About : UserControl
         e.Handled = true;
     }
     #endregion URL click
+
+    #region ReadMe click
+    /// <summary>
+    /// Handles the Click event of the BtnReadMe control.
+    /// </summary>
+    /// <param name="sender">
+    /// The source of the event.
+    /// </param>
+    /// <param name="e">
+    /// The <see cref="RoutedEventArgs" /> instance containing the event data.
+    /// </param>
+    private void BtnReadMe_Click(object sender, RoutedEventArgs e)
+    {
+        string dir = AppInfo.AppDirectory;
+        TextFileViewer.ViewTextFile(Path.Combine(dir, "ReadMe.txt"));
+    }
+    #endregion ReadMe click
 }
